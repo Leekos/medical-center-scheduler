274c2728e5af63b56f5a62040a7749fc8f1989c2
Tomasz <tomsankowski@gmail.com>
2022-04-19 13:25:56 +0200

    Final final

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -151,7 +151,6 @@ function LoginForm({ Login, error }) {
                   variant="contained"
                   color="primary"
                   type="submit"
-                  variant="contained"
                   fullWidth
                 >
                   Log in
@@ -165,39 +164,3 @@ function LoginForm({ Login, error }) {
   );
 }
 export default LoginForm;
-// <form onSubmit={submitHandler}>
-//   <div className="form-inner">
-//     <h2>Sign in</h2>
-//     {error != "" ? <div className="error">{error}</div> : ""}
-//     <div className="form-group">
-//       <label htmlFor="name">Name:</label>
-//       <input
-//         type="text"
-//         name="name"
-//         id="name"
-//         onChange={(e) => setDetails({ ...details, name: e.target.value })}
-//         value={details.name}
-//       />
-//     </div>
-//     <div className="form-group">
-//       <label htmlFor="email">E-mail:</label>
-//       <input
-//         type="email"
-//         name="email"
-//         id="email"
-//         onChange={(e) => setDetails({ ...details, email: e.target.value })}
-//         value={details.email}
-//       />
-//     </div>
-//     <div className="form-group">
-//       <label htmlFor="password">Hasło:</label>
-//       <input
-//         type="password"
-//         name="password"
-//         id="password"
-//         onChange={(e) =>
-//           setDetails({ ...details, password: e.target.value })
-//         }
-//         value={details.password}
-//       />
-//     </div>
